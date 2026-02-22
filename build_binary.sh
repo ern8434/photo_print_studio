858a0c41faffc045cebe9be8f2e8bc85605ff971
Eren Ezgü <erenezgu@gmail.com>
2026-02-22 20:10:50 +0300

    build güncellendi

--- build_binary.sh
+++ build_binary.sh
@@ -1,3 +1,5 @@
-pyinstaller --noconfirm --onefile --windowed --icon=logo.png  \
+pyinstaller --noconfirm --onefile --windowed \
+--hidden-import "PIL._tkinter_finder" \
 --add-data ".venv/lib/python3.12/site-packages/customtkinter:customtkinter/" \
+--add-data "logo.png:." \
 main.py
